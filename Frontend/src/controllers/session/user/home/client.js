5edbb394c9416b5ca09dd0ff85654982ef56afb5
Chris Pettyjohn <cpettyjohn@pchs.k12.il.us>
2018-01-22 18:53:40 -0600

    Arcturus Push 1.01
    -Refer to general 1.01 build commit
    -Officially changed to "production"

--- Frontend/src/controllers/session/user/home/client.js
+++ Frontend/src/controllers/session/user/home/client.js
@@ -16,39 +16,39 @@ class Client
     {
       this.$http.get('/api/auth/sso')
         .then (sso => {
-          var flashvars = 
+          var flashvars =
           {
-            "connection.info.host"                    : "104.198.202.77",
+            "connection.info.host"                    : "35.226.240.137",
             "connection.info.port"                    : "3000",
             "url.prefix"                              : "/me",
             "site.url"                                : "/me",
             "client.reload.url"                       : "/me",
             "client.fatal.error.url"                  : "/client",
             "client.connection.failed.url"            : "/me",
-            "external.variables.txt"                  : "http://vanitygaming.ca/swfs/gamedata/variables.txt",
-            "external.texts.txt"                      : "http://vanitygaming.ca/swfs/gamedata/texts.txt",
-            "productdata.load.url"                    : "http://vanitygaming.ca/swfs/gamedata/productdata.txt",
-            "furnidata.load.url"                      : "http://vanitygaming.ca/swfs/gamedata/furnidata.xml",
-            "external.figurepartlist.txt"             : "http://vanitygaming.ca/swfs/gamedata/figuredata.xml",
-            "external.override.texts.txt"             : "http://vanitygaming.ca/swfs/gamedata/override/texts.txt",
-            "external.override.variables.txt"         : "http://vanitygaming.ca/swfs/gamedata/override/variables.txt",
-            "external.figurepartlist.txt"             : "http://vanitygaming.ca/swfs/gamedata/figuredata.xml",
+            "external.variables.txt"                  : "http://swfs.xhabbo.fun/gamedata/variables.txt",
+            "external.texts.txt"                      : "http://swfs.xhabbo.fun/gamedata/texts.txt",
+            "productdata.load.url"                    : "http://swfs.xhabbo.fun/gamedata/productdata.txt",
+            "furnidata.load.url"                      : "http://swfs.xhabbo.fun/gamedata/furnidata.xml",
+            "external.figurepartlist.txt"             : "http://swfs.xhabbo.fun/gamedata/figuredata.xml",
+            "external.override.texts.txt"             : "http://swfs.xhabbo.fun/gamedata/override/texts.txt",
+            "external.override.variables.txt"         : "http://swfs.xhabbo.fun/gamedata/override/variables.txt",
+            "external.figurepartlist.txt"             : "http://swfs.xhabbo.fun/gamedata/figuredata.xml",
             "client.starting.revolving"               : "Already!?",
             "use.sso.ticket"                          : "1",
             "sso.ticket"                              : sso.data.auth_ticket,
             "processlog.enabled"                      : "0",
-            "flash.client.url"                        : "http://vanitygaming.ca/swfs/other/game/",
+            "flash.client.url"                        : "http://swfs.xhabbo.fun/other/game/",
             "flash.client.origin"                     : "popup",
             "client.allow.cross.domain"               : "1",
             "client.notify.cross.domain"              : "0",
           };
           var params =
           {
-            "base" : "http://vanitygaming.ca/swfs/other/game/",
+            "base" : "http://swfs.xhabbo.fun/other/game/",
             "allowScriptAccess" : "always",
             "menu" : "false"
           };
-          swfobject.embedSWF("http://vanitygaming.ca/swfs/gamedata/habbo.swf", "client", "100%", "100%", "10.0.0", "", flashvars, params, null);
+          swfobject.embedSWF("http://swfs.xhabbo.fun/gamedata/arcturus.swf", "client", "100%", "100%", "10.0.0", "", flashvars, params, null);
         })
     }
 

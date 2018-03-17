239508b0ecbdbea33f85fa8dda61a6ca80c7fb54
Chris Pettyjohn <chrismpettyjohn@gmail.com>
2018-03-16 23:12:39 -0500

    Frontend Merge
    -Error handling improvement
    -Moves error views to /system seperate from both frontend (client) and backend (admin)
    -Adds another errors.message that can retrieve a message paramater with title and content and display it
    -Web service now refreshes every 10 minutes

--- Frontend/src/services/settings.js
+++ Frontend/src/services/settings.js
@@ -1,27 +1,35 @@
 export default class Settings {
-  constructor($http, $rootScope, $localStorage, $interval) {
+  constructor($http, $state, $rootScope, $localStorage, $interval) {
     'ngInject'
     // Website Settings
-    $http.get('/api/config/website')
-      .then(website => {
-        $rootScope.website = website.data
-        $localStorage.website = website.data
-      })
-      .catch(error => {
-        return null
-      })
+
+    const fetch_web = () => {
+      $http.get('/api/config/website')
+        .then(website => {
+          $rootScope.website = website.data
+          $localStorage.website = website.data
+        })
+        .catch(error => {
+          $state.go('errors.500')
+        })
+    }
+
     // Online Users
-    const count = () => {
+    const fetch_users = () => {
       $http.get('/api/users/online/count')
         .then(users => {
           $rootScope.website.online = users.data
         })
         .catch(error => {
-          return null
+          $state.go('errors.500')
         })
     }
-    count()
-    $interval(count, 2500)
+
+    fetch_web()
+    fetch_users()
+
+    $interval(fetch_users, 2500)
+    $interval(fetch_web, 10000)
   }
 
 
